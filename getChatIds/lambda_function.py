84e7505d74d421689237c68c0f3fd9cf9cbeee00
VeganL <veganlroy@gmail.com>
2023-05-31 15:46:27 -0400

    20230531 1546: GHADA-EDITED ENDPOINT (changes transferred from AWS to Github)

--- getChatIds/lambda_function.py
+++ getChatIds/lambda_function.py
@@ -24,27 +24,27 @@ def getChat(chatdata):
             return 'Error: cid does not exist'
 
         else:
+            #print(response['Items'])
             chatInfo = response['Items'][0]
             room = chatInfo['roomName']
+            chunkCreateTimestamp = chatInfo['chunkCreateTimestamp']
             msglen = len(chatInfo['messageList'])
             temp = {}
-            if msglen == 0:
-                temp.update({'cid': x})
-                temp.update({'roomName': room})
-            else:
+            temp.update({'cid': x})
+            temp.update({'roomName': room})
+            temp.update({'chunkCreateTimestamp': chunkCreateTimestamp})
+            if msglen != 0:
                 lastMSG = chatInfo['messageList'][msglen-1]
-                temp.update({'cid': x})
-                temp.update({'roomName': room})
-                temp.update({'messageList': lastMSG})
+                temp.update({'firstMessage': lastMSG})
             getOutPut.append(temp)
 
     return getOutPut
 
-
 def lambda_handler(event, context):
     # TODO implement
     data = event['multiValueQueryStringParameters']['chatroomCids[]']
-    # data = event['chatroomCids']
+    #data = event['queryStringParameters']['chatroomCids']
+    #print(event['queryStringParameters'])
     sendBack = getChat(data)
 
     try:
